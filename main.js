9a021e6bda48733b880fe4d2769edb0b3bc5b737
jordanmosongo <mosongojordan@gmail.com>
2021-04-23 10:19:10 +0200

    finition de la section service et projet

--- main.js
+++ main.js
@@ -1,78 +1,100 @@
 window.onscroll = () => {
-  let scrollPoint = document.body.scrollTop || document.documentElement.scrollTop;
+  let scrollPoint =
+    document.body.scrollTop || document.documentElement.scrollTop;
 
-  if(scrollPoint <= 799){
+  if (scrollPoint <= 799) {
     activateLinkOnScroll(0);
-  }else if(scrollPoint >= 800 && scrollPoint <= 1915) {
+  } else if (scrollPoint >= 800 && scrollPoint <= 1915) {
     activateLinkOnScroll(1);
-  }else if(scrollPoint >= 1916 && scrollPoint <= 3047){
+  } else if (scrollPoint >= 1916 && scrollPoint <= 3047) {
     activateLinkOnScroll(2);
-  }else if(scrollPoint >= 3050 && scrollPoint <= 4099){
+  } else if (scrollPoint >= 3050 && scrollPoint <= 4099) {
     activateLinkOnScroll(3);
-  }else if(scrollPoint > 4099){
+  } else if (scrollPoint > 4099) {
     activateLinkOnScroll(4);
   }
-  
-}
-function activateLinkOnScroll(i){
-  let itemsLink = document.getElementsByClassName('menu-link__item');
-  for(let itemLink of itemsLink){
-    itemLink.classList.remove('active');
+};
+function activateLinkOnScroll(i) {
+  let itemsLink = document.getElementsByClassName("menu-link__item");
+  for (let itemLink of itemsLink) {
+    itemLink.classList.remove("active");
   }
-  itemsLink[i].classList.add('active');
+  itemsLink[i].classList.add("active");
 }
 
-$(document).ready(function(){
-  $('select').formSelect();
-  $('.sidenav').sidenav();
-  $('.modal').modal();
-  $('.carousel.carousel-slider').carousel({
-    fullWidth: true
+$(document).ready(function () {
+  $("select").formSelect();
+  $(".sidenav").sidenav();
+  $(".modal").modal();
+  $(".carousel.carousel-slider").carousel({
+    fullWidth: true,
   });
 });
-(function(){
-  window.addEventListener('load', () => {
+(function () {
+  window.addEventListener("load", () => {
     AOS.init({
       duration: 1000,
       easing: "ease-in-out",
       once: true,
-      mirror: false
-  });
-  })
-})();
-(function() {
-  window.addEventListener('scroll', () => {
-      AOS.init({
-          duration: 1000,
-          easing: "ease-in-out",
-          once: true,
-          mirror: false
-      });  
+      mirror: false,
+    });
   });
 })();
-//Animmation with gsap 
-const tl = gsap.timeline({default: { ease : "power1.out"}});
-tl.to('.banner-text__title__span', {
-  y:"0%",
-  duration : 1,
-  stagger : 0.75
+(function () {
+  window.addEventListener("scroll", () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  });
+})();
+//Animmation with gsap
+const tl = gsap.timeline({ default: { ease: "power1.out" } });
+tl.to(".banner-text__title__span", {
+  y: "0%",
+  duration: 1,
+  stagger: 0.75,
 });
-tl.fromTo('.banner-text__description', 
-            {opacity : 0},
-            {opacity : 1, duration : 1}
-          );
-tl.fromTo('.banner-text__bouton', 
-          {opacity : 0},
-          {opacity : 1, duration : 1}
-        );
-function activateLink(event){
+tl.fromTo(
+  ".banner-text__description",
+  { opacity: 0 },
+  { opacity: 1, duration: 1 }
+);
+tl.fromTo(".banner-text__bouton", { opacity: 0 }, { opacity: 1, duration: 1 });
+function activateLink(event) {
   let linkStrigger = event.target;
-  let itemsLink = document.getElementsByClassName('menu-link__item');
-  for(let item of itemsLink){
-    item.classList.remove('active');
+  let itemsLink = document.getElementsByClassName("menu-link__item");
+  for (let item of itemsLink) {
+    item.classList.remove("active");
   }
-  linkStrigger.classList.add('active');
+  linkStrigger.classList.add("active");
+}
+let cardLinks = document.getElementsByClassName("card-link");
+let cardProjects = document.getElementsByClassName("card");
 
+for (let i = 0; i < cardProjects.length; i++) {
+  cardProjects[i].addEventListener("mouseenter", (event) => {
+    event.preventDefault();
+    console.log(cardLinks);
+    cardLinks[i].style.display = "inherit";
+  });
+  cardProjects[i].addEventListener("mouseleave", (event) => {
+    event.preventDefault();
+    cardLinks[i].style.display = "none";
+  });
 }
 
-
+// {
+//     "title" : "Formulaire de contact",
+//     "description" : "Une application frontEnd qui gère les contacts",
+//     "image" : "../images/realisation/formulaireOne.png",
+//     "technologies" : [
+//         "Html5",
+//         "css3",
+//         "javascript"
+//     ],
+//     "lienSite" : "https://lien-vers-le-site.com",
+//     "lienGitHub" : "https://lien-vers-le-repo-git"
+// }
--- service.js
+++ service.js
@@ -1,125 +1,124 @@
 //skill variables
-let description = document.getElementById('text-descriptif');
-let telephone = document.getElementById('telephone');
-let facebook = document.getElementById('facebook');
+let description = document.getElementById("text-descriptif");
+let telephone = document.getElementById("telephone");
+let facebook = document.getElementById("facebook");
 let firstRowOfSkills = document.getElementById("first-row-of-skills");
 let secondRowOfSkills = document.getElementById("second-row-of-skills");
 //project variables
 let projectContainer = document.getElementById("projects-container");
-let secondProjectContainer = document.getElementById('projects-container-second');
+let secondProjectContainer = document.getElementById(
+  "projects-container-second"
+);
 
-function createSkill(parent, skillTechnology, skillImage){
-    let skillContainer = document.createElement('div');
-    let firstBloc = document.createElement('div');
-    let secondBloc = document.createElement('div');
-    let skillTech = document.createElement('img');    
-    let skillName = document.createElement('p');
+function createSkill(parent, skillTechnology, skillImage) {
+  let skillContainer = document.createElement("div");
+  let firstBloc = document.createElement("div");
+  let secondBloc = document.createElement("div");
+  let skillTech = document.createElement("img");
+  let skillName = document.createElement("p");
 
-    skillContainer.classList.add("col", "s4");    
-    firstBloc.classList.add('skill__element');
-    secondBloc.classList.add('skill__name');
-    skillTech.setAttribute('src', skillImage);
-    skillTech.setAttribute('alt', skillTechnology);
-    skillName.textContent = skillTechnology;
+  skillContainer.classList.add("col", "s4");
+  firstBloc.classList.add("skill__element");
+  secondBloc.classList.add("skill__name");
+  skillTech.setAttribute("src", skillImage);
+  skillTech.setAttribute("alt", skillTechnology);
+  skillName.textContent = skillTechnology;
 
-    firstBloc.appendChild(skillTech);
-    skillContainer.append(firstBloc, secondBloc, skillName);
-    parent.appendChild(skillContainer);
+  firstBloc.appendChild(skillTech);
+  skillContainer.append(firstBloc, secondBloc, skillName);
+  parent.appendChild(skillContainer);
 }
-function createProject(parent,projectTitle ,projectImage, technoList, siteWeb, gitHub){
-     
+const createCardImage = (image, index) => {
+  let cardImage = document.getElementsByClassName("card-image");
+  let imageForCard = document.createElement("img");
+  imageForCard.setAttribute("src", `${image}`);
+  imageForCard.setAttribute("alt", "image projet");
+  cardImage[index].appendChild(imageForCard);
+};
+const createCardContent = (title, description, index) => {
+  let cardContent = document.getElementsByClassName("card-content");
+  let titre = document.createElement("h3");
+  let descriptionText = document.createElement("p");
+  descriptionText.textContent = description;
+  titre.classList.add("card-title");
+  titre.textContent = title;
+  cardContent[index].append(titre, descriptionText);
+};
+const createCardAction = (arrayOfTechnologies) => {
+  let technologiesListElement = document.createElement("ul");
+  arrayOfTechnologies.forEach((technology) => {
+    let technologyElement = document.createElement("li");
+    technologyElement.textContent = technology;
+    technologiesListElement.appendChild(technologyElement);
+  });
+  return technologiesListElement;
+};
+const createCardLink = (lienSite, index) => {
+  let cardLink = document.getElementsByClassName("card-link__bloc");
+  let lien = document.createElement("a");
+  lien.classList.add("card-image__link");
+  lien.setAttribute("href", `${lienSite}`);
+  lien.textContent = "visiter";
+  cardLink[index].appendChild(lien);
+};
+
+function createProject(project, index) {
+  const { title, description, image, technologies, lienSite } = project;
+  createCardImage(image, index);
+  createCardContent(title, description, index);
+  let cardAction = document.getElementsByClassName("card-action");
+  cardAction[index].appendChild(createCardAction(technologies));
+  createCardLink(lienSite, index);
 }
 
 //getting identity
 //fetch("https://my-json-server.typicode.com/jordanmosongo/portfolio/identity")
-fetch("http://localhost:3000/identity")  
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        telephone.textContent = data.telephone;
-        facebook.setAttribute('href', data.facebook);
-        
-    })
-    .catch(err => console.log(err));
+fetch("http://localhost:3000/identity")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    telephone.textContent = data.telephone;
+    facebook.setAttribute("href", data.facebook);
+  })
+  .catch((err) => console.log(err));
 //getting skills
 //fetch("https://my-json-server.typicode.com/jordanmosongo/portfolio/skills")
-fetch("http://localhost:3000/skills")     
-    .then(response => {
-            return response.json();
-    })
-    .then(arrayOfSkills => {
-        console.log(arrayOfSkills);
-        for(let index in arrayOfSkills){
-            if(index <= 2) {
-                createSkill(firstRowOfSkills, arrayOfSkills[index].technology, 
-                            arrayOfSkills[index].logo);
-            } else if (index >2 && index <= 5) {
-                createSkill(secondRowOfSkills, arrayOfSkills[index].technology, 
-                    arrayOfSkills[index].logo);
-            }else{
-                alert("En attente d'algorithme");
-            }
-        }
-
-    })
-    .catch(err => console.log(err));
+fetch("http://localhost:3000/skills")
+  .then((response) => {
+    return response.json();
+  })
+  .then((arrayOfSkills) => {
+    for (let index in arrayOfSkills) {
+      if (index <= 2) {
+        createSkill(
+          firstRowOfSkills,
+          arrayOfSkills[index].technology,
+          arrayOfSkills[index].logo
+        );
+      } else if (index > 2 && index <= 5) {
+        createSkill(
+          secondRowOfSkills,
+          arrayOfSkills[index].technology,
+          arrayOfSkills[index].logo
+        );
+      } else {
+        alert("En attente d'algorithme");
+      }
+    }
+  })
+  .catch((err) => console.log(err));
 //getting projects
 fetch("http://localhost:3000/projects")
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        for(let index in data){
-            if(index <= 2){
-                createProject(projectContainer, data[index].title, data[index].image,
-                    data[index].technologies, data[index].lienSite, data[index].lienGitHub);
-            }else if(index >=3 && index <=5){
-                createProject(secondProjectContainer, data[index].title, data[index].image,
-                    data[index].technologies, data[index].lienSite, data[index].lienGitHub);
-            }
-            else{
-                alert("En attente d'algorithme")
-            }
-            
-        }
-    })
-    .catch(err => console.log(err));
-
-    //Sending mails
-    let submitButton = document.getElementById("bouton-submit");
-    let email = document.getElementById("email");
-    let objet = document.getElementById("sujet");
-    let message = document.getElementById("message");
-    let nom = document.getElementById("nom");
-    let phone = document.getElementById("phone");
-    submitButton.addEventListener('click', event => {
-        event.preventDefault();
-        Email.send({
-            Host : "smtp.mailtrap.io",
-            Username: "4b96413af87a4f",
-            Password: "d2dc011c0cabbd",
-            To : "jordan@gmail.com",
-            From : email.value,
-            Subject : objet.value,
-            Body : message.value + '<br>' + nom.value + '<br>' + phone.value
- 
-        })
-        .then(msg => {
-            alert("Votre message est envoyé avec succès");
-            let form = document.getElementById('form');
-            form.reset();
-        })
-    })
-    function unroll(){
-        let email = document.getElementById('email-inscription');
-        console.log(email.value);
-        fetch('http://localhost:3000/newsletter',email.value, {
-            method:'post'
-        }).then(result => {
-            console.log(result);
-        })
-        .catch(err => console.log(err));
-    }
-
-
+  .then((response) => {
+    return response.json();
+  })
+  .then((projects) => {
+    // projects.forEach((project) => {
+    //   createProject(project);
+    // });
+    projects.map((project, index) => {
+      createProject(project, index);
+    });
+  })
+  .catch((err) => console.log(err));
